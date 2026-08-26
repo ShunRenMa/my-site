8e7324b7997b494f348ee82dcec39addf1a4780b
Shun Ren Ma <e12813726@gmail.com>
2026-08-26 17:40:31 +0800

    加入一些tagline

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,28 +1,36 @@
 <script setup>
-const message = 'Hello from the script setup!'
 const layoutClass = 'layout'
 </script>
 
 <template>
 	<div :class="layoutClass">
-		<section class="hero">
-			<img
-				src="~/assets/css/cover1.jpg"
-				alt=""
-				class="hero__img"
-				fetchpriority="high"
-			/>
+		<section class="hero block">
+			<div class="hero_content">
+				<span class="hero_title_fst">Made In Taiwan.</span>
+				<span class="hero_title_sec">Building For The World.</span>
+			</div>
+			<div class="hero_img_wrapper">
+				<img
+					src="~/assets/css/cover1.jpg"
+					alt="hero image"
+					class="hero__img"
+					fetchpriority="high"
+				/>
+			</div>
 		</section>
+
+		<div class="profile block"></div>
 	</div>
 </template>
 
 <style scoped>
-.hero {
-	/* 用 % 不用 100vw，vw 會把捲軸寬度算進去而多出橫向捲軸 */
+.block {
+	display: block;
 	width: 100%;
-	/* 第一行給不支援 dvh 的舊瀏覽器墊底 */
-	height: 100vh;
-	height: 100dvh;
+	height: 105vh;
+	height: 105dvh;
+}
+.hero {
 	overflow: hidden;
 }
 
@@ -32,5 +40,34 @@ const layoutClass = 'layout'
 	/* 填滿容器、多的裁掉，所以任何比例的視窗都是滿版 */
 	object-fit: cover;
 	display: block;
+}
+.hero_img_wrapper {
+	width: 100%;
+	height: 100%;
+	overflow: hidden;
+	position: relative;
+	z-index: 1000;
+}
+
+.hero_content {
+	position: absolute;
+	font-size: 4vw;
+	left: 4vw;
+	top: 4vw;
+	z-index: 1001;
+	text-align: left;
+	color: white;
+	user-select: none;
+	letter-spacing: 0.2rem;
+}
+
+.hero_content span {
+	display: block;
+}
+
+.profile {
+	width: 100%;
+	height: 100vh;
+	background-color: transparent;
 }
 </style>
--- app/plugins/lenis.client.ts
+++ app/plugins/lenis.client.ts
@@ -1,18 +1,18 @@
-
-import Lenis from 'lenis';
+import Lenis from 'lenis'
 import 'lenis/dist/lenis.css'
 
 export default defineNuxtPlugin((nuxtApp) => {
+	if (isDesktopDevice()) {
+		const lenis = new Lenis({
+			autoRaf: true,
+		})
 
-  if(isDesktopDevice()) {
-    const lenis = new Lenis({
-      autoRaf: true,
-    });
-
-    // lenis.on('scroll', (e) => {
-    //   console.log(e);
-    // });
-  }
+		lenis.on('scroll', (e) => {
+			// console.log(e.scroll) // 目前捲動的 px
+			// console.log(e.limit) // 可捲動的最大值（極限值，等同 scrollHeight - innerHeight）
+			// console.log(e.progress) // 0~1，已經是 clamp 好的整頁進度
+			// console.log(e.velocity) // 捲動速度
+			// console.log(e.direction) // 1 或 -1，方向
+		})
+	}
 })
-
-

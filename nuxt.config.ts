d83342c43e157ce9999df3fa70fd3c02ed7db013
Shun Ren Ma <e12813726@gmail.com>
2026-09-05 00:48:29 +0800

    add seo

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -10,13 +10,26 @@ export default defineNuxtConfig({
 			meta: [
 				{
 					property: 'og:site_name',
-					content: 'Baird Ma / Frontengineer / Photographyer',
+					content: 'Baird Ma / Frontend Engineer / Photographer',
 				},
 				{ property: 'og:type', content: 'website' },
 				{ property: 'og:locale', content: 'zh_TW' },
 				{ name: 'twitter:card', content: 'summary_large_image' },
 			],
-			link: [{ rel: 'icon', href: '/favicon.ico' }],
+			link: [
+				{ rel: 'icon', href: '/favicon.ico', sizes: '16x16 32x32' },
+				{
+					rel: 'icon',
+					type: 'image/png',
+					href: '/favicon-32.png',
+					sizes: '32x32',
+				},
+				{
+					rel: 'apple-touch-icon',
+					href: '/apple-touch-icon.png',
+					sizes: '180x180',
+				},
+			],
 		},
 	},
 })
